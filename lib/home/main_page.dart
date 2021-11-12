f39eb0d0d03d17560e5d8142478a2a9496fc9ca6
Christ Henry Loha <cristloha2205@gmail.com>
2021-11-12 15:52:51 +0800

    update

--- lib/home/main_page.dart
+++ lib/home/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:crypto_app/shared/theme.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatelessWidget {
@@ -11,8 +12,35 @@ class MainPage extends StatelessWidget {
         child: Container(
           width: double.infinity,
           height: 60,
-          color: Colors.red,
-          decoration: BoxDecoration(),
+          margin: EdgeInsets.only(
+            bottom: 30,
+            left: defaultMargin,
+            right: defaultMargin,
+          ),
+          decoration: BoxDecoration(
+            color: primaryColor,
+            borderRadius: BorderRadius.circular(17),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconBottomNavigation(
+                image: 'assets/icon_profile.png',
+              ),
+              IconBottomNavigation(
+                image: 'assets/icon_news.png',
+              ),
+              IconBottomNavigation(
+                image: 'assets/icon_dashboard.png',
+              ),
+              IconBottomNavigation(
+                image: 'assets/icon_collection.png',
+              ),
+              IconBottomNavigation(
+                image: 'assets/icon_help.png',
+              ),
+            ],
+          ),
         ),
       );
     }
@@ -26,6 +54,36 @@ class MainPage extends StatelessWidget {
           customBottomNavigation(),
         ],
       ),
+    );
+  }
+}
+
+class IconBottomNavigation extends StatelessWidget {
+  final String image;
+  const IconBottomNavigation({
+    Key? key,
+    required this.image,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 16,
+        ),
+        Container(
+          width: 24,
+          height: 24,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                image,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
